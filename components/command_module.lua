50611daeaa85f840eba009367838b110467fedf4
David-Liddelow <liddelow165@gmail.com>
2019-10-05 21:57:02 +1000

    command_module.lua fix

--- components/command_module.lua
+++ components/command_module.lua
@@ -7,7 +7,7 @@ local conf = {
     imageOrigin = {670, 500},
     imageScale = {0.1, 0.1},
     activationOrigin = {0, 0},
-    shapeCoords = {25,0, 10,10, 10,-18, -38,-15, -38,15},
+    shapeCoords = { 20,15, 20,-20, -38,-20, -38,15},
     density = 1,
     name = "command module",
 }
