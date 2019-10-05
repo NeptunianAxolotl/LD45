8b7f54c02f26205bc5fec36e78ed2961c01353ef
David-Liddelow <liddelow165@gmail.com>
2019-10-05 19:09:48 +1000

    added play on

--- components/player.lua
+++ components/player.lua
@@ -3,7 +3,7 @@ local FORCE = 180
 
 local conf = {
     imageOff = "images/player.png",
-    imageOn = "images/player.png",
+    imageOn = "images/player on.png",
     imageOrigin = {670, 500},
     imageScale = {0.1, 0.1},
     activationOrigin = {0, 0},
