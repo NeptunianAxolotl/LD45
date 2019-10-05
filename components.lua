a0f76e774b3d4c7f67be0b93363d20451cb1cd31
David-Liddelow <liddelow165@gmail.com>
2019-10-05 20:12:12 +1000

    added asteroid 1

--- components.lua
+++ components.lua
@@ -4,6 +4,7 @@ components.booster = require("components/booster")
 components.tractor_wheel = require("components/tractor_wheel")
 components.player = require("components/player")
 components.girder1 = require("components/girder 1")
+components.girder1 = require("components/asteroid 1")
 
 -- Post processing
 local compList = {}
