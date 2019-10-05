941e5f194888c64e83525526f89ff9472baed5c7
Evan Wallace <mankarse.git@alethrea.net>
2019-10-05 15:35:30 +1000

    Increased window size.

--- conf.lua
+++ conf.lua
@@ -1,7 +1,9 @@
 function love.conf(t)
     t.version = "11.0"
-
     t.window.title = "LD45"
-    
+
     t.console = true
+    t.window.title = "Katamari Freelancer"
+    t.window.width = 1024
+    t.window.height = 768
 end
