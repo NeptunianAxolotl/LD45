4003f8c91aa93806965d7a9d5c36eb01ff5b3350
David-Liddelow <liddelow165@gmail.com>
2019-10-05 18:32:51 +1000

    added player

---
+++ components/player.lua
@@ -0,0 +1,19 @@
+
+local FORCE = 180
+
+local conf = {
+    imageOff = "images/booster.png",
+    imageOn = "images/booster on.png",
+    imageOrigin = {670, 500},
+    imageScale = {0.1, 0.1},
+    activationOrigin = {0, 0},
+    shapeCoords = {25,0, 10,20, 10,-20, -20,-20, -20,20},
+    mass = 20,
+    name = "booster1",
+    onFunction = function (self, body, activeX, activeY, activeAngle)
+        local fx, fy = FORCE*math.cos(activeAngle), FORCE*math.sin(activeAngle)
+        body:applyForce(fx, fy, activeX, activeY)
+    end,
+}
+
+return conf
