3df1cbb398aaf569413ee9d57843715f774e4418
GoogleFrog <googl3frog@gmail.com>
2019-10-06 21:11:23 +1100

    Fixed push missile rocket.

--- components/push_missile.lua
+++ components/push_missile.lua
@@ -1,5 +1,5 @@
 
-local FORCE = 4200
+local FORCE = 20000
 
 local conf = {
     imageOff = "images/push_missile/push_missile_unused.png",
@@ -9,7 +9,7 @@ local conf = {
     imageScale = {0.5, 0.5},
     activationOrigin = {0, 40},
     shapeCoords = {15,-35, 7,-44, -7,-44, -15,-35, -15,21, -16,26, 16,26, 15,21},
-    density = 1,
+    density = 10,
     text =
     {
         pos = {5, 0},
@@ -19,7 +19,8 @@ local conf = {
     },
     holdActivate = true,
     onFunction = function (self, body, activeX, activeY, activeAngle)
-        local fx, fy = -FORCE*math.sin(activeAngle), FORCE*math.cos(activeAngle)
+        activeAngle = activeAngle - math.pi*0.5
+        local fx, fy = FORCE*math.cos(activeAngle), FORCE*math.sin(activeAngle)
         body:applyForce(fx, fy, activeX, activeY)
     end,
 }
