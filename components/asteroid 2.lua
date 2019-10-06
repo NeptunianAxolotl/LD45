9bf8e884f854290ed6c28d2acdbbcf2e8811e09c
David-Liddelow <liddelow165@gmail.com>
2019-10-06 12:12:20 +1100

    added additional asteroid

---
+++ components/asteroid 2.lua
@@ -0,0 +1,13 @@
+local conf = {
+    imageOff = "images/asteroid 2.png",
+    imageOn = "images/asteroid 2.png",
+    imageOrigin = {500, 500},
+    imageScale = {0.1, 0.1},
+    activationOrigin = {0, 0},
+    circleShapeRadius = 25,
+    density = 1,
+    noAttach = true,
+    noSelect = true,
+}
+
+return conf
