2218e5a600ecfac4410e7d4ee33c064c4c204b8a
Aquanim <threeoneteacup@gmail.com>
2019-10-06 19:25:59 +1100

    Rewrote starfield

--- starfield.lua
+++ starfield.lua
@@ -1,39 +1,97 @@
 local me = {}
 
-local closeCross = 3 -- how many screens of movement are required to move the closest stars from one side of the screen to the other
-local farCross = 15 -- how many screens of movement are required to move the furthest stars from one side of the screen to the other
-local fieldWidth = 2 -- how many screens' worth of stars are generated
+local lprime = 1031
+local fields = {{bsize = 128, minf = 10, maxf = 25, probs = {0.9,0.6,0.3,0.1}},{bsize = 128, minf = 25, maxf = 100, probs = {0.5,0.2,0.1}},{bsize = 128, minf = 100, maxf = 1000, probs = {0.25,0.05}}}
+local winkProb = 0.25
 
-local star = {}
-for i = 1, 200 do
-  star[#star + 1] = {x = math.random() * fieldWidth, y = math.random() * fieldWidth, z = math.random()} 
+local function starsInBox(bx,by,field)
+
+  local bsize = field.bsize
+  local probs = field.probs
+
+  r = 1021 * bx + 929 * by + field.minf + 7 * field.maxf
+  
+  for i = 1,10 do
+	r = (137 * r + 61) % lprime
+  end
+  
+  local density = r / lprime
+  
+  local nstars = 0
+  for i = 1, #probs do
+	if density < probs[i] then nstars = nstars + 1 end
+  end
+  
+  local stars = {}
+  for i = 1, nstars do
+    r = (137 * r + 61) % lprime
+	while r > math.floor(lprime/bsize)*bsize do r = (137 * r + 61) % lprime end
+	sx = r % bsize
+	
+	r = (137 * r + 61) % lprime
+	while r > math.floor(lprime/bsize)*bsize do r = (137 * r + 61) % lprime end
+	sy = r % bsize
+	
+	r = (137 * r + 61) % lprime
+	sz = r / lprime
+	
+	r = (137 * r + 61) % lprime
+	sm = r / lprime
+	
+	stars[#stars + 1] = {x=sx,y=sy,z=sz,mag=sm}
+  end
+
+  return stars
 end
 
-local function projection(px, py, star)
-  local prj = {}
-  prj.x = (star.x - px / (star.z * farCross + (1-star.z) * closeCross)) % (fieldWidth)
-  prj.y = (star.y - py / (star.z * farCross + (1-star.z) * closeCross)) % (fieldWidth)
-  return prj
-end
 
-local function locations(px, py)
+local function locations(px, py, cameraScale)
+
+  cameraScale = cameraScale or 1
+  
+  local cutoff = (1 - 1 / (cameraScale * cameraScale)) * 0.99
+  local margin = math.min(0.07,(1-cutoff)*0.9)
   
   local winWidth  = love.graphics:getWidth()
   local winHeight = love.graphics:getHeight()
-  local largestDim = math.max(winWidth,winHeight)
-  local xRatio = winWidth / largestDim
-  local yRatio = winHeight / largestDim
-  
-  local vx = px / largestDim
-  local vy = py / largestDim
   
   local onscreenStars = {}
-  for i = 1, #star do
-    local prj = projection(vx, vy, star[i])
-    if prj.x < xRatio and prj.y < yRatio then
-      onscreenStars[#onscreenStars+1] = {math.floor(prj.x * largestDim), math.floor(prj.y * largestDim), 1, 1, 1, 1}
-    end
+  for f = 1, #fields do
+	  local bsize = fields[f].bsize
+  
+  	  local x1 = math.floor((px/fields[f].maxf - (winWidth/2) * cameraScale) / bsize)
+	  local x2 = math.ceil((px/fields[f].maxf + (winWidth/2) * cameraScale) / bsize)
+	  
+	  local y1 = math.floor((py/fields[f].maxf - (winHeight/2) * cameraScale) / bsize)
+	  local y2 = math.ceil((py/fields[f].maxf + (winHeight/2) * cameraScale) / bsize)
+	  
+	  
+	  for i = x1, x2 do
+		for j = y1, y2 do
+			stars = starsInBox(i,j,fields[f])
+			for s = 1, #stars do
+				local sx = bsize * i + stars[s].x
+				local sy = bsize * j + stars[s].y
+				local sz = stars[s].z
+				local sm = stars[s].mag
+				
+				local prjx = (sx - px/(sz*fields[f].maxf + (1-sz)*fields[f].minf)) / cameraScale + winWidth/2
+				local prjy = (sy - py/(sz*fields[f].maxf + (1-sz)*fields[f].minf)) / cameraScale + winHeight/2
+				
+				local bright = 0
+				if sm > cutoff + margin then bright = 1
+				elseif sm > cutoff - margin then bright = (sm-(cutoff-margin))/(margin*2)
+				end
+				
+				if bright > 0.05 then 
+					local light = (math.random() < winkProb) and bright*0.7 or ((math.random() < winkProb) and math.min(bright*1.3,1) or bright)
+					onscreenStars[#onscreenStars+1] = {prjx, prjy, light, light, light, 1}
+				end
+			end
+		end
+	  end
   end
+
   return onscreenStars
 end
 
