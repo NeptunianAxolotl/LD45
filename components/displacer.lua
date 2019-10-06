be8d730944b531af83ae04b6ce1fa9dc4e3a5d7f
Istaera <istaera@gmail.com>
2019-10-06 13:57:25 +1100

    displacer

---
+++ components/displacer.lua
@@ -0,0 +1,18 @@
+local conf = {
+    imageOff = "images/displacer.png",
+    imageOn = "images/displaceron.png",
+    imageOrigin = {500, 550},
+    imageScale = {0.1, 0.1},
+    activationOrigin = {0, 0},
+    shapeCoords = {-30,-15, 30,-15, 30,5, 12.5,15, -12.5,15, -30,5},
+    density = 1,
+    text =
+    {
+        pos = {-5, 5},
+        rotation = 0,
+        scale = {1.5, 1.5},
+        color = {0.8,0.1,0.1,1},
+    },
+}
+
+return conf
