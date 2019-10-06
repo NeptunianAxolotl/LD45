a747e49ce58497beffdb14f9ea55ea3c6d829479
Istaera <istaera@gmail.com>
2019-10-06 17:24:34 +1100

    debris burner

---
+++ components/debris_burner.lua
@@ -0,0 +1,18 @@
+local conf = {
+    imageOff = "images/debrisburner.png",
+    imageOn = "images/debrisburneron.png",
+    imageOrigin = {500, 700},
+    imageScale = {0.1, 0.1},
+    activationOrigin = {0, 0},
+    shapeCoords = {-38,-30, 38,-30, 38,20, -38,20},
+    density = 2,
+    text =
+    {
+        pos = {-5, 5},
+        rotation = 0,
+        scale = {1.5, 1.5},
+        color = {0.8,0.1,0.1,1},
+    },
+}
+
+return conf
