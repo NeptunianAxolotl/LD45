bc97d01c29a7ee708d1a5a3d23a16c65bdc90095
David-Liddelow <liddelow165@gmail.com>
2019-10-06 11:47:13 +1100

    fixed gun hit box

--- components/gun.lua
+++ components/gun.lua
@@ -1,10 +1,10 @@
 local conf = {
     imageOff = "images/gun.png",
     imageOn = "images/gun.png",
-    imageOrigin = {600, 400},
+    imageOrigin = {600, 420},
     imageScale = {0.2, 0.2},
     activationOrigin = {0, 0},
-    shapeCoords = { 25,5, 25,-5, -25,-5, -25,5},
+    shapeCoords = { 45,5, 45,-5, -50,-5, -50,5},
     density = 1,
 }
 
