ee0bd70092a6fd8d177ea5bb64ddd160f0ab34a4
will-f-au <heavy.lancer@gmail.com>
2019-10-06 11:29:41 +1100

    added push_missile & push_missile debris

---
+++ components/push_missile debris.lua
@@ -0,0 +1,14 @@
+
+local FORCE = 180
+
+local conf = {
+    imageOff = "images/push_missile/push_missile_launch_debris.png",
+    imageOn = "images/push_missile/push_missile_launch_debris.png",
+    imageOrigin = {30, 17},
+    imageScale = {1, 1},
+    activationOrigin = {0, 0},
+    shapeCoords = { 30,17, -30,17, -30,17, 30,-17},
+    density = 1,
+}
+
+return conf
