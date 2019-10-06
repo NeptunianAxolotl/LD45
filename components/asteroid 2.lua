9ab86859ba92f47247851f285a69d704d048bba0
Aquanim <threeoneteacup@gmail.com>
2019-10-06 13:00:38 +1100

    Improved asteroid 2 hitbox

--- components/asteroid 2.lua
+++ components/asteroid 2.lua
@@ -1,10 +1,10 @@
 local conf = {
     imageOff = "images/asteroid 2.png",
     imageOn = "images/asteroid 2.png",
-    imageOrigin = {500, 500},
+    imageOrigin = {487, 435},
     imageScale = {0.1, 0.1},
     activationOrigin = {0, 0},
-    circleShapeRadius = 25,
+    shapeCoords = { -32,0, -26,-14, -4,-32, 23,-28, 33,-10, 13,28, -8,32, -28,29 },
     density = 1,
     noAttach = true,
     noSelect = true,
