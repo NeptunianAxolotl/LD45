061b2e17913dba2b40bab980267a0f78de0450fa
GoogleFrog <googl3frog@gmail.com>
2019-10-06 17:15:40 +1100

    Fix command module.

--- components/command_module.lua
+++ components/command_module.lua
@@ -1,7 +1,7 @@
 local conf = {
     imageOff = "images/command module.png",
     imageOn = "images/command module.png",
-    imageDmg = {"images/command module damage","images/command module damage 2"},
+    imageDmg = {"images/command module damage.png","images/command module damage 2.png"},
     imageOrigin = {598, 466},
     imageScale = {0.1, 0.1},
     activationOrigin = {0, 0},
